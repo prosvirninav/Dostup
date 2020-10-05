4d2ab9100797e889ea7754692094917c2bf0d45f
prosvirninav@mail.ru <prosvirninav@mail.ru>
2020-10-06 00:45:30 +0300

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,26 +1,46 @@
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 3,
+$(document).ready(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-})
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
 
+  var modalButton = $('[data-toggle="modal"]');
+  var closeModalButton = $(".close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
 
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+});
 
 
 
